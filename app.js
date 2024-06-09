f2b1fcfbf45da3c896fc1f455d3763cc672bb9e8
Aditya Shahi <ashahi1@ualberta.ca>
2024-06-09 15:53:13 -0600

    inital testing

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const admin = require('firebase-admin');
+const serviceAccount = require('./legalease-34703-firebase-adminsdk-c8iwm-7abfa54524.json');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,3 +18,24 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
+db.collection('test').add({ hello: 'world' })
+  .then(docRef => console.log('Document written with ID: ', docRef.id))
+  .catch(error => console.error('Error adding document: ', error));
+  app.get('/test-firestore', (req, res) => {
+    const db = admin.firestore();
+    db.collection('test').add({ hello: 'world' })
+        .then(docRef => {
+            console.log('Document written with ID: ', docRef.id);
+            res.send(`Document written with ID: ${docRef.id}`); // This line sends a response back to the browser
+        })
+        .catch(error => {
+            console.error('Error adding document: ', error);
+            res.status(500).send('Failed to write document'); // Sends an error message back to the browser
+        });
+});
+
